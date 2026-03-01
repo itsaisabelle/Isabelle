385223bd2a421b7f0a91d793b3fe4ec65c7db54f
itsaisabelle <isabelle.d.campeau@gmail.com>
2026-03-01 00:46:33 +0000

    admin user story 4

--- movies/admin.py
+++ movies/admin.py
@@ -14,9 +14,10 @@ admin.site.register(Report)
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ("id","user", "comment", "movie", "date", "reported")
-    list_filter = ("comment",)
 
     def commentCounter(self, obj):
         return obj.comment.count()
 
     commentCounter.short_description = 'Number of Comments'
+    list_filter = ("coummentCounter",)
+
